72c2f7f9797147dd018945f174aae54a4201f269
wolve324 <111057273+wolve324@users.noreply.github.com>
2025-04-21 21:59:06 +0530

    Update videoTester.py

--- videoTester.py
+++ videoTester.py
@@ -1,8 +1,8 @@
 import os
 import cv2
 import numpy as np
-from keras.models import model_from_json
-from keras.preprocessing import image
+from tensorflow.keras.models import model_from_json
+from tensorflow.keras.preprocessing import image
 def exp():
     #load model
     model = model_from_json(open("fer.json", "r").read())
